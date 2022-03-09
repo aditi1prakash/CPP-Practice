7c51e873fe90899a81c28d6d8c14bacd41e769d6
Aditi Prakash <aditi1prakash@gmail.com>
2022-03-09 15:42:56 +0100

    S16 - Polymorphism

---
+++ S16_Challenge_Polymorphism/myCode/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+
+// Main program
+int main ()
+{
+    // TODO: Add your program code here
+	cout << "S16_Challenge_Polymorphism started." << endl << endl;
+
+	return 0;
+}
