2c55caad60bb3abee2427830fcaf8e02805d4264
Aditi Prakash <aditi1prakash@gmail.com>
2022-05-16 14:32:34 +0200

    iomaip challenge solution

---
+++ S19_Challenge_iomanip/myCode/README.md
@@ -0,0 +1,12 @@
+# Section 19 Challenge - I/O Manipulation #
+
+In this challenge you are to display the provided data in a nicely formatted table. 
+
+## Problem Description ##
+
+There are 3 structures provided. The structures work together to create a Tours structure that will contain information about Tours to South America. 
+The tours include Countries, Cities within those countries along with population and cost data for each city tour.
+
+In the provided source code, there is some code that displays the data from the structures and the vectors within the structures. This code uses tabs to format the data and it looks pretty ugly.
+
+Have fun! Create functions if you like, use constants, whatever you think makes sense!
--- S19_Challenge_iomanip/myCode/main.cpp
+++ S19_Challenge_iomanip/myCode/main.cpp
@@ -1,12 +1,107 @@
 #include <iostream>
 #include <cstdlib>
 
+#include <vector>
+#include <string>
+#include <iomanip>
 
 using namespace std;
+
+struct City
+{
+	std::string name;
+	long population;
+	double cost;
+};
+
+//Assume each country has at least 1 city
+struct Country
+{
+	std::string name;
+	std::vector<City> cities;
+};
+
+struct Tours
+{
+	std::string title;
+	std::vector<Country> countries;
+};
 
 
 int main ()
 {
+	/* Build the structure based on Uniform Initialization */
+	Tours tours
+	{
+		"Tour ticket prices from Berlin",
+		{
+				{
+					"Colombia", {
+						{ "Bogota", 8778000, 400.98},
+						{ "Cali", 2401000, 424.12},
+						{ "Medellin", 2464000, 350.98},
+						{ "Cartagena", 972000, 345.34}
+					},
+
+				},
+				{
+					"Brazil", {
+						{ "Rio de Janeiro", 13500000, 567.45},
+						{ "Sao Paulo", 11310000, 975.45},
+						{ "Salvador", 18234000, 855.99}
+					},
+				},
+				{
+					"Chile", {
+						{"Valdivia", 260000, 569.12},
+						{"Santiago", 7040000, 520.00}
+					},
+				},
+				{
+					"Argentina", {
+						{"Buenos Aires", 3010000, 728.77}
+					}
+				},
+		}
+	};
+
+	cout << "12345678901234567890123456789012345678901234567890123456789012345678901234567890" << endl << endl;
+
+	/* Display title */
+	cout << setw(55);
+	cout << right << tours.title << endl << endl;
+
+	/* Display columns */
+	cout << setw(20) << left << "Country"
+		 << setw(20) << left << "City"
+		 << setw(20) << right << "Population"
+		 << setw(20) << right << "Cost" << endl;
+
+	/* Display divider */
+	char emptychar{' '};
+    cout << setfill ('-') << setw(81) << emptychar << endl;
+
+    cout << setfill(' ');
+
+	for (auto country: tours.countries)
+	{
+		cout << setw(20) << left << country.name;
+		bool firstCity{true};
+
+		for (auto city: country.cities)
+		{
+			if (!firstCity)
+			{
+				cout << setw(20) << ' ';
+			}
+			cout << setw(20) << left << city.name
+				 << setw(20) << right << city.population
+				 << setw(20) << right << city.cost << endl;
+
+			firstCity = false;
+		}
+	}
+
 
 	return 0;
 }
