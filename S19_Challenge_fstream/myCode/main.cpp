6977851bea2f3ae7c2a65cfe3cdec9adf37ea8a9
Aditi Prakash <aditi1prakash@gmail.com>
2022-05-22 20:06:06 +0200

    section 19 file streams v2

--- S19_Challenge_fstream/myCode/main.cpp
+++ S19_Challenge_fstream/myCode/main.cpp
@@ -8,9 +8,7 @@
 
 using namespace std;
 
-std::string solutionString{};
-
-int calculateScore (std::string studentSolution)
+int calculateScore (std::string studentSolution, std::string solutionString)
 {
 	int score {};
 
@@ -28,8 +26,34 @@ int calculateScore (std::string studentSolution)
 	return score;
 }
 
+void printHeader()
+{
+	//Formatting for printing the output on the console
+	cout << setw(20) << left << "Student"
+		 << setw(20) << "Score" << endl;
+}
+
+void printDivider()
+{
+	cout << setw(40) << setfill ('-') << "";
+	cout << setfill (' ') << " " << endl;
+}
+
+void printStudent(std::string studentName, int score)
+{
+	cout << setw(20) << left << studentName
+		 << setw(20) << left << score << endl;
+}
+
 int main ()
 {
+	//Local variables to store student name and their solution
+	std::string studentName{};
+	std::string studentSolution{};
+	std::string solutionString{};
+	int studentCount{}, sum{};
+	double average{};
+
 	// Open file and connect it to filestream object
 	std::ifstream in_file{"responses.txt"};
 
@@ -39,57 +63,41 @@ int main ()
 		std::cerr << "Error opening file" << std::endl;
 		return -1;
 	}
-
-	//Student database
-	std::vector<std::pair<std::string, int>> studentData;
 	
 	//Get the solution string
 	getline(in_file, solutionString);
 
-	//Local variables to store student name and their solution
-	std::string studentName{};
-	std::string studentSolution{};
-	float sum{}, average{};
+	//Formatting
+	printHeader();
+	printDivider();
 
 	//Read from file
-	while (!in_file.eof())
+	while (in_file >> studentName >> studentSolution)
 	{
-		getline(in_file, studentName);			//Get student name
-		getline(in_file, studentSolution);		//Get student solution
+		//Increment number of student
+		++studentCount;
 
 		//Calculate score of each student
-		int score = calculateScore(studentSolution);
+		int score = calculateScore(studentSolution, solutionString);
 
-		//Score the record in the database
-		studentData.push_back(std::make_pair(studentName, score));
-	}
+		//Calculate sum
+		sum += score;
 
-	//Formatting for printing the output on the console
-	cout << setw(20) << left << "Student" 
-		 << setw(20) << "Score" << endl;
-
-	cout << setw(40) << setfill ('-') << "";
-	cout << setfill (' ') << " " << endl;
-
-	//Print the student name and respective score
-	for (size_t i = 0; i < studentData.size(); i++)
-	{
-		cout << setw(20) << left << studentData[i].first 
-			 << setw(20) << left << studentData[i].second << endl;
-
-		//Calculate sum of scores
-		sum += studentData[i].second;		
+		//Print on the console
+		printStudent(studentName, score);
 	}
 
 	//Calculate the average
-	average = sum/studentData.size();
+	if(studentCount != 0)
+	{
+		average = static_cast<double>(sum)/studentCount;
 
-	//Formatting
-	cout << setw(40) << setfill ('-') << "";
-	cout << setfill (' ') << " " << endl;
+		//Formatting
+		printDivider();
 
-	//Print the average
-	cout  << "Average = " << average << endl;
+		//Print the average
+		cout  << "Average = " << average << endl;
+	}
 
 	//Close the file
 	in_file.close();
