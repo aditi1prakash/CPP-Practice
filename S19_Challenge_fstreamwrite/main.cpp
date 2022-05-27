e33c3154c34fba510e82d13ad9ea73984219775f
Aditi Prakash <aditi1prakash@gmail.com>
2022-05-27 21:48:06 +0200

    S19 ofstream v2

--- S19_Challenge_fstreamwrite/main.cpp
+++ S19_Challenge_fstreamwrite/main.cpp
@@ -5,10 +5,12 @@
 #include <iomanip>
 
 int main ()
-{
+{   
+    // Associate stream object to input and output files
     std::fstream out_file{"romeoandjuliet-copy.txt"};
     std::fstream in_file{"romeoandjuliet.txt"};
 
+    // Error handling
     if(!in_file)
     {
         std::cerr << "Error opening input file" << std::endl;
@@ -18,16 +20,22 @@ int main ()
         std::cerr << "Error opening output file" << std::endl;
     }
 
+    // local variables
     int lineNumber{};
     std::string line{};
 
+    // Read each line, format, and copy
     while(getline(in_file, line))
     {
-        if (line.size() > 0)
+        if (line.size() > 0)    // or - if(line != "")
             out_file << std::setw(5) << std::left << ++lineNumber << line << std::endl;
         else
             out_file << line << std::endl;
     }
+
+    // Close file streams
+    in_file.close();
+    out_file.close();
 
     return 0;
 }
